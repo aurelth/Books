b42f18b9c2300efda8fece2d34916e4fad484f6a
aurelth <aurelth@icloud.com>
2020-08-28 15:30:44 -0300

    DataTables and Dele API Call

--- BookListRazor/Controllers/BookController.cs
+++ BookListRazor/Controllers/BookController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
-using BookListRazor.Model;
-using Microsoft.AspNetCore.Mvc;
 
 namespace BookListRazor.Controllers
 {
@@ -19,9 +18,22 @@ namespace BookListRazor.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data = _db.Book.ToList()});
+            return Json(new { data = await _db.Book.ToListAsync()});
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(b => b.Id == id);
+            if(bookFromDb == null)
+            {
+                return Json(new { success=false, message="Error while Deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success=true, message="Delete Successfull"});
         }
     }
 }
